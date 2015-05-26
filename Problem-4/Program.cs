cc48d38c0ba506d2e2323b9093b56b9f0462b1b8
Alex-Curran <alexcurran839@gmail.com>
2015-05-25 20:44:49 -0500

    Set up

---
+++ Problem-4/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ *   Project Euler: Problem 4
+ *   Alex Curran 
+ *   
+ *   A palindromic number reads the same both ways. The largest palindrome made from the
+ *   product of two 2-digit numbers is 9009 = 91 × 99.
+ *   Find the largest palindrome made from the product of two 3-digit numbers.
+ *   
+ *  https://projecteuler.net/problem=4
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
